f172cc1e779f0af4d5a09aeb926c7d816a0dbf2d
mayawilli <nikeprosandbigbows23@gmail.com>
2018-08-09 15:10:56 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,23 @@
 #write out your code here
 
 def least_coins(cents)
+  
+  quar = cents/25
+  money_left = cents%25
+  dim = money_left/10 
+  money_left= money_left%10
+  nick=money_left/5 
+  money_left= money_left%5 
+  penn = money_left
+  coins= {:quarters => quar, :dimes => dim, :nickels => nick , :pennies => penn}
+  return coins 
 
-#Code your answer here!
+# coins {}
+# coins [:quarters] = 7
+# coins [:dimes] = 1 
+# coins [:nickels] = 
+# coins  [:pennies] = 4
 
 end
+
+puts least_coins(93)
